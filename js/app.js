151e67d323339b9a8955d81a7044179ea4861afc
Abdalla-Rabeh <abdallahcodeing@gmail.com>
2024-07-01 04:43:12 +0300

    first commit

--- js/app.js
+++ js/app.js
@@ -1,22 +1,22 @@
 $(document).ready(function(){
-    $(".logo").fadeOut(5000);
+  $(".logo").fadeOut(5000);
 });
-    
+  
 
 var scroll_top =$('.Scroll-top')
 $(window).scroll(function(){
-    if($(window).scrollTop() >= 100){
-        if(scroll_top.is(':hidden')){
-        $(scroll_top).fadeIn(400)
-        }
-    }else{
-        $(scroll_top).fadeOut(400)
-    }
+  if($(window).scrollTop() >= 100){
+      if(scroll_top.is(':hidden')){
+      $(scroll_top).fadeIn(400)
+      }
+  }else{
+      $(scroll_top).fadeOut(400)
+  }
 })
 $(".scroll_top").click(function(e){
-    e.preventDefault();
-    $('html, body').animate({
-        scrollTop:0
-        
-    },1000)
+  e.preventDefault();
+  $('html, body').animate({
+      scrollTop:0
+      
+  },1000)
 })
